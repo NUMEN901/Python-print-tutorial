fe868f61eff7560a73671870f61fe42271032d92
Junior NUMEN <numenjun@gmail.com>
2025-06-18 16:39:20 +0200

    Add thi changes

--- greetings_v2.py
+++ greetings_v2.py
@@ -1,6 +1,4 @@
 import os
 text_file= open("Greetings2.txt", "w")
-#print("fuck you", file= files)
-#files.close()
 print("Good morning my people", "Good afternoon my people", "Good night my people", sep=os.linesep, file=text_file)
 text_file.close()
